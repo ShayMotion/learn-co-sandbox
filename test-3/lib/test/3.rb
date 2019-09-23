b3265db38387e2ea1fe3d11f980ed8f34e44fb86
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-23 19:43:03 +0000

    first commit

---
+++ test-3/lib/test/3.rb
@@ -0,0 +1,8 @@
+require "test/3/version"
+
+module Test
+  module 3
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test-3/lib/test/3/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module 3
+    VERSION = "0.1.0"
+  end
+end
