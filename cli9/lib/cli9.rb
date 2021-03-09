121268bf3f7d581596de35d247a56d9837a7eb87
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-09 22:52:02 +0000

    Automatically backed up by Learn

--- cli9/lib/cli9.rb
+++
@@ -1,9 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require_relative "./cli9/version.rb"
-require_relative './cli9/cli.rb'
-require_relative './cli9/Custom_Guitars.rb'
-require_relative './cli9/scraper.rb'
-
-
--- cli9/lib/cli9/version.rb
+++
@@ -1,3 +0,0 @@
-module GuitarGallery
-  VERSION="0.1.0"
-end
